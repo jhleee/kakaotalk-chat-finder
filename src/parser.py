d90b42b499b483b18ecfc79744fe95e62880145d
jhleee <ng0301@gmail.com>
2023-10-04 16:42:49 +0900

    Fix: parsing content containing newline.
    대화 내용 중 개행이 있는 경우 처리

--- src/parser.py
+++ src/parser.py
@@ -21,23 +21,39 @@ def parse(filename, max_line=None):
 # Extract chats from the given content
 def parse_text(content):
     chats = []
+    chat_date_pattern = re.compile(r"\d{4}년 \d{1,2}월 \d{1,2}일 .요일")
+    chat_pattern = re.compile(r"\[(.*?)\] \[(오전|오후) (\d{1,2}:\d{1,2})\]")
+    system_message_pattern = re.compile(r".+님이 들어왔습니다\.|.+님이 나갔습니다\.")
+
+    current_date = None
+    current_speaker = None
+    current_time = None
+    current_message = []
+
+    chats = []
     current_date = None
 
-    # Define regex patterns
-    date_pattern = re.compile(r"--------------- (\d{4}년 \d{1,2}월 \d{1,2}일) .+? ---------------")
-    chat_pattern = re.compile(r"\[(.+?)\] \[\w+? (\d{1,2}:\d{1,2})\] (.+)")
-
+    # Iterate through the content and extract chats
     for line in content:
-        date_match = date_pattern.match(line)
-        chat_match = chat_pattern.match(line)
-
-        if date_match:  # Update the current date if a date line is found
-            date_str = date_match[1]
+        # Check if the line has the date format
+        date_match = re.search(r"\d{4}년 \d{1,2}월 \d{1,2}일", line)
+        if date_match:
+            date_str = date_match.group()
             current_date = datetime.strptime(date_str, "%Y년 %m월 %d일").date()
-        elif chat_match and current_date:  # Extract chat details if a chat line is found
-            speaker = chat_match[1]
-            time_str = chat_match[2]
-            message = chat_match[3]
+            continue
+
+        # Check for system messages and skip them
+        if "님이 들어왔습니다." in line or "님이 나갔습니다." in line:
+            continue
+
+        # Extract chat messages
+        chat_match = re.search(r"\[(.*?)\] \[(.*?)\] (.*)", line)
+        if chat_match:
+            speaker, _, message = chat_match.groups()
             chats.append(Chat(current_date, speaker, message))
+        else:
+            # If the current line is not a new chat, it's a continuation of the previous message
+            if chats:
+                chats[-1].message += "\n" + line.strip()
 
     return chats
